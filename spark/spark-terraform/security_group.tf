451cdb937e1714340de8335400ae1851b80f1f51
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2023-07-10 06:30:55 +0000

    Change cidr_blocks to allow all inbound traffic on port 22

--- spark/spark-terraform/security_group.tf
+++ spark/spark-terraform/security_group.tf
@@ -5,10 +5,11 @@ resource "aws_security_group" "emr_security_group" {
   vpc_id      = aws_vpc.emr_vpc.id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = [aws_vpc.emr_vpc.cidr_block]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   egress {
@@ -19,13 +20,6 @@ resource "aws_security_group" "emr_security_group" {
   }
 
   depends_on = [aws_subnet.emr_subnet]
-
-  lifecycle {
-    ignore_changes = [
-      ingress,
-      egress,
-    ]
-  }
 
   tags = {
     name = "emr_security_group"
