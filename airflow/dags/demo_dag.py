ac83d52596556a024f8ed90cec976c9926a98e5e
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2023-08-02 06:46:05 +0000

    Add a schedule and task dependencies

--- airflow/dags/demo_dag.py
+++ airflow/dags/demo_dag.py
@@ -1,18 +1,51 @@
 from airflow.decorators import dag, task
 import logging
-from datetime import datetime
+import datetime
+import os
 
 @dag(
-    schedule=None,
-    start_date=datetime.now(),
-    catchup=False,
+    schedule='@daily',
+    start_date=datetime.datetime.now() - datetime.timedelta(days=4),
     tags=['demo']
 )
 def demo_dag():
+    
     @task()
-    def log_hello_world():
+    def hello_world():
         logging.info('Hello, World!')
         
-    log_hello_world = log_hello_world()
-
+    hello_world = hello_world()
+    
+    @task()
+    def current_time():
+        logging.info(f"Current time is {datetime.datetime.utcnow().isoformat()}")
+        
+    current_time = current_time()
+    
+    @task()
+    def working_dir():
+        logging.info(f"Working directory is {os.getcwd()}")
+        
+    working_dir = working_dir()
+    
+    @task()
+    
+    def complete():
+        logging.info("DAG completed")
+        
+    complete = complete()
+    
+    # Task dependencies should look like this:
+    #
+    #                -> current_time
+    #               /               \
+    #   hello_world                  -> complete
+    #               \               /
+    #                 -> working_dir
+    
+    hello_world >> current_time
+    hello_world >> working_dir
+    current_time >> complete
+    working_dir >> complete
+    
 demo_dag()
